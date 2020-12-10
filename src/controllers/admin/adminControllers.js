4198ef06f5770bf814d203ed1a83131bb73fe92a
NISHIPATODI <nishi.patodi@gmail.com>
2020-12-10 16:26:43 +0530

    abc

--- src/controllers/admin/adminControllers.js
+++ src/controllers/admin/adminControllers.js
@@ -10,13 +10,69 @@ const {
   const Teacher = require("../../models/teacherModel");
   var nodemailer = require('nodemailer');
 
-  const InviteLink = async (req, res) => {
+  const InviteLinkStud = async (req, res) => {
     console.log(req.body);
     //if(req.body.userType==="student")
     let { branch,year,section} = req.body;
     //coding_hustlers_vaibhav
     let [err, user_inserted] = await to(
         Student.query().select().where("year",year).andWhere("branch",branch).andWhere("section",section)
+        .returning("*")
+      );
+      
+      if (err) badRequestError(res, "unable to fetch user");
+    
+      //delete user_inserted.password;
+      console.log("USER's detail ", user_inserted);
+     // return okResponse(res, "user inserted successfully");
+     
+    
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'nishi.patodi@gmail.com',
+
+          pass: "Nishi@9425"
+        }
+      });
+      let mail="sandeepsinghgour0@gmail.com";
+      console.log("USER's detail inside mail function ", user_inserted.length);
+     
+
+
+          for(let i=0;i<user_inserted.length;i++){
+      var mailOptions = {
+        from: "nishi.patodi@gmail.com",
+        to: user_inserted[i].email,
+        subject: 'Link For Test',
+        text: `  emailId is ${user_inserted[i].email } password is ${user_inserted[i].password} /n}is `
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          return okResponse(res, "mailSent succesfully");
+     
+        }
+      });
+    }
+      
+//res.send("Mail sent")
+
+}
+
+const InviteLinkTeach = async (req, res) => {
+    console.log(req.body);
+    //if(req.body.userType==="student")
+    let { email,fullName} = req.body;
+
+    console.log(email);
+    
+    //coding_hustlers_vaibhav
+    let [err, user_inserted] = await to(
+        Student.query().select().where("email",email).andWhere("fullName",fullName)
         .returning("*")
       );
       
@@ -86,7 +142,8 @@ const UserDetails = async (req, res) => {
 
 
 module.exports = {
-    InviteLink,
-    UserDetails
-
+    InviteLinkStud,
+    UserDetails,
+    InviteLinkTeach
+    
 }
